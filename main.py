f917a95baa34e90ad99c6e245675c3c086be1dcb
Italo Chinchay <italo.chinchay@udep.edu.pe>
2023-11-30 19:58:08 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Connecting to the ACI Server:
+This ACI Sandbox Lab contains a shared ACI Server Instance.
+The server can be connected using login credentials
+user: admin
+password : !v3G@!4@Y
+url = https://sandboxapicdc.cisco.com
+"""
+import requests
+import json
+import conf
+
+print(conf.usuario)
+print(conf.clave)
